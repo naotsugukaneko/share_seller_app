58552df543b3bf6f76bf781c17c1a474facadf6c
naotsugukaneko <nkk.407@gmail.com>
2021-08-15 01:04:04 +0900

    activeadminに必要なファイルをインストールしマイグレーションを実施

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@ User.destroy_all
 
 User.create!(name: "テスト", email: "test@example.com", password: "password")
 puts "ユーザーの初期データインポートに成功しました。"
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
