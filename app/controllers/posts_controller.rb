8b68906eec4e7388e74eb834f2f4accd1c234fa1
naotsugukaneko <nkk.407@gmail.com>
2021-08-17 23:26:39 +0900

    削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
+
   def index
     @posts = Post.includes(:user).order(created_at: :desc)
   end
@@ -20,11 +22,18 @@ class PostsController < ApplicationController
 
   # def update; end
 
-  # def destroy; end
+  def destroy
+    @post.destroy!
+    redirect_to root_path
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:body, :image)
+  end
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
   end
 end
