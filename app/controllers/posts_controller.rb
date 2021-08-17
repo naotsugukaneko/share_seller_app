6f3b173109e1b16e16ca944f7ec8ed64c51f61a8
naotsugukaneko <nkk.407@gmail.com>
2021-08-17 16:25:41 +0900

    一覧ページのN+1問題を解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.includes(:user).order(created_at: :desc)
   end
 
   def new
