ec0bcd67aa6ae5b1d0925acf506a3169f6aa9fc4
naotsugukaneko <nkk.407@gmail.com>
2021-08-22 23:57:29 +0900

    エラーの解消

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,13 @@ class CommentsController < ApplicationController
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     @comment.save
-    render :index
+    render :_index
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    render :index
+    render :_index
   end
 
   private
