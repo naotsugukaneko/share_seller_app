ad137d860909c6e76bf0b6e548abce2505bf2ab8
naotsugukaneko <nkk.407@gmail.com>
2021-09-02 23:51:12 +0900

    likesコントローラーの作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+  before_action :post_params
+
+  def create
+    current_user.likes.create!(post_id: @post.id)
+  end
+
+  def destroy
+    current_user.likes.find_by(post_id: @post.id).destroy!
+  end
+
+  private
+
+  def post_params
+    @post = Post.find(params[:post_id])
+  end
 end
