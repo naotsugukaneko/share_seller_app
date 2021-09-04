60110447e8a829bac0578a5d6a150d1ceb97cf43
naotsugukaneko <nkk.407@gmail.com>
2021-09-04 11:34:01 +0900

    Userモデルにliked_by?メソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def liked_by?(post_id)
+    likes.where(post_id: post_id).exists?
+  end
 end
