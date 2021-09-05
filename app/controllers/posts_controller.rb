110181175f1d899ea2168feeb45384f57a1643f0
naotsugukaneko <nkk.407@gmail.com>
2021-09-05 13:14:05 +0900

    postsコントローラーでページネーションを設定,定数はモデルに定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[edit update destroy]
 
   def index
-    @posts = Post.includes(:user, :likes).order(created_at: :desc)
+    @posts = Post.includes(:user, :likes).order(created_at: :desc).page(params[:page]).per(Movie::PER)
   end
 
   def new
