157e146f42552a95ede46699553fd2beb3e1319d
naotsugukaneko <nkk.407@gmail.com>
2021-09-10 00:35:34 +0900

    マイページの投稿した記事を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   def show
+    @posts = @user.posts.order(created_at: :desc)
     @likes = Like.where(user_id: @user.id).order(created_at: :desc)
   end
 
