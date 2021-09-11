bccdfdfba10c7ec989d3c247c395fd5eb65f4657
naotsugukaneko <nkk.407@gmail.com>
2021-09-11 22:44:47 +0900

    ゲストログインのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root "posts#index"
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   resources :users, only: [:show]
   resources :posts do
     resources :comments, only: [:create, :destroy]
