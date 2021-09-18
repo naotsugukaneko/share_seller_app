73b7bc376db90f62a1e73921d6a5cfcc9e179531
naotsugukaneko <nkk.407@gmail.com>
2021-09-19 00:19:57 +0900

    画像のリサイズを再設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,7 +27,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w[jpg jpeg gif png]
   end
 
-  process resize_to_limit: [400, 500]
+  process resize_to_limit: [400, 400]
 
   def size_range
     0..5.megabytes
