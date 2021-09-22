ffe2dcc438c1af276a679211bd550ecbf81a75b3
naotsugukaneko <nkk.407@gmail.com>
2021-09-22 23:57:03 +0900

    ajax通信を用いてjson形式でコントローラーに渡せるように実装

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -4,5 +4,14 @@ $(document).on('turbolinks:load', function () {
     //このアプリケーション(document)の、formというid('#form')で、キーボードが押され指が離れた瞬間(.on('keyup'...))、eという引数を取って以下のことをしなさい(function(e))
     e.preventDefault(); //キャンセル可能なイベントをキャンセル
     var input = $.trim($(this).val()); //この要素に入力された語句を取得し($(this).val())、前後の不要な空白を取り除いた($.trim(...);)上でinputという変数に(var input =)代入
+    $.ajax({
+      //ajax通信で以下のことを行います
+      url: '/posts/search', //urlを指定
+      type: 'GET', //メソッドを指定
+      data: 'keyword=' + input, //コントローラーに渡すデータを'keyword=input(入力された文字のことですね)'にするように指定
+      processData: false, //おまじない
+      contentType: false, //おまじない
+      dataType: 'json', //データ形式を指定
+    });
   });
 });
